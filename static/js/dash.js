e6b5119cb4b2c28aea2d2277157be7239e34526f
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-04-14 16:09:03 +0000

    Modified dashboard

--- static/js/dash.js
+++ static/js/dash.js
@@ -518,14 +518,6 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-const hamburgerBtn = document.getElementById('hamburger-btn');
-const sidebar = document.getElementById('sidebar');
-
-hamburgerBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('collapsed');
-  hamburgerBtn.classList.toggle('active');
-});
-
 
 document.addEventListener("DOMContentLoaded", () => {
 // Initialize data fetching on page load
